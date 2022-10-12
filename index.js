0a4b185ccb9a97cbbfc2f07a8b39ea2e963cc471
Alexandre Salvador <62520840+alexandresalvador@users.noreply.github.com>
2022-10-12 13:04:43 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const express = require('express');
-
 const app = express();
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const DB = {
     artigos: [
         {
-            id: 100,
+            id: 1,
             titulo: "Revolução Agrícola",
             categoria: "historia",
             desc: "As Revoluçôes Agrícolas foram as transformações que ocorreram na cultura agrícola que influenciaram na história da humanidade que migraram do sistema de caça[...]",
             data: "1/10/2022",
-            hora: "",
+            hora: "13:20",
             autor: "Ramón",
         },
         {
-            id: 101,
+            id: 2,
             titulo: "Futuro da Política",
             categoria: "científica",
             desc: "A juventude brasileira está inconformada com o país em que vive. Afastada dos partidos e da política, pouco quer saber dos fundamentos da economia e do[...]",
             data: "2/3/2022",
-            hora: "",
+            hora: "14:56",
             autor: "Luis",
         },
         {
-            id: 102,
+            id: 3,
             titulo: "Evasão Escolar",
             categoria: "científica",
             desc: "Cerca de 244 mil crianças e adolescentes entre 6 e 14 anos estavam fora da escola no segundo trimestre de 2021. Esse número representa[..]",
             data: "5/9/2022",
-            hora: "",
+            hora: "15:30",
             autor: "Carlos",
         },
     ],
     autores: [
         {
-            id: 100,
+            id: 1,
             nome: "Luis",
             email: "luis.123@gmail.com",
             gravatar: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gratispng.com%2Fpng-htlez1%2F&psig=AOvVaw3aHR-IMDsaBdXxs7069vTF&ust=1665599507910000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCZ0bjo2PoCFQAAAAAdAAAAABAD",
@@ -44,7 +44,7 @@ const DB = {
             categoria: "literatura",
         },
         {
-            id: 101,
+            id: 2,
             nome: "Ramón",
             email: "ramon.103@gmail.com",
             gravatar: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gratispng.com%2Fpng-htlez1%2F&psig=AOvVaw3aHR-IMDsaBdXxs7069vTF&ust=1665599507910000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCZ0bjo2PoCFQAAAAAdAAAAABAD",
@@ -52,7 +52,7 @@ const DB = {
             categoria: "ficção",
         },
         {
-            id: 102,
+            id: 3,
             nome: "Carlos",
             email: "carlos.343@gmail.com",
             gravatar: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gratispng.com%2Fpng-htlez1%2F&psig=AOvVaw3aHR-IMDsaBdXxs7069vTF&ust=1665599507910000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCZ0bjo2PoCFQAAAAAdAAAAABAD",
@@ -63,28 +63,46 @@ const DB = {
 };
 
 
-//Aqui é o home
+//Rota que retorna na Home
 app.get('/', function (req, res) {
     res.render('index');
 });
 
-//// criando uma rota que retorna todos os artigos
+
+// ----------- READ
+
+//// Criando uma rota que retorna todos os artigos por meio do método GET.
 app.get('/artigos', function (req, res) {
-    response.send(DB.artigos);
-    res.render('artigos');
+     res.send(DB.artigos);
+    //res.render('artigos');
 });
-
-// criando uma rota que retorna todos os autores
+// Criando uma rota que retorna todos os autores por meio do método GET.
 app.get('/autores', function (req, res) {
-    response.send(DB.autores);
-    res.render('autores');
+     res.send(DB.autores);
+    //res.render('autores');
 });
 
-// app.get('/autores/id', function (req, res) {
-//     res.render('autoresId');
-// });
 
-// criando uma rota que retorna um autor por id
+// criando uma rota que retorna um artigo por id por meio do método GET.
+app.get("/artigos/:artigoId", (req, res) => {
+    const idArtigo = req.params.artigoId;
+    if (isNaN(idArtigo)) {
+         res.statusCode = 400;
+         res.send("O id informado não é um número.");
+    } 
+    else {
+      const id = parseInt(idArtigo);
+      const artigo = DB.artigos.find((index) => index.id === id);
+            if (artigo !== undefined) {
+                res.statusCode =  200;
+                res.json(artigo) ;
+                 } else {
+                    res.sendStatus(404);
+                 }
+         }
+   });
+
+// criando uma rota que retorna um autor por id por meio do método GET.
 app.get("/autores/:autorId", (req, res) => {
     const idAutor = req.params.autorId;
     if (isNaN(idAutor)) {
@@ -94,32 +112,179 @@ app.get("/autores/:autorId", (req, res) => {
     else {
       const id = parseInt(idAutor);
       const autor = DB.autores.find((index) => index.id === id);
-      if (autor !== undefined) {
-             res.statusCode =  200;
-             res.json(autor) ;
-           } else {
-             res.sendStatus(404);
+            if (autor !== undefined) {
+                  res.statusCode =  200;
+                  res.json(autor) ;
+                 } else {
+                     res.sendStatus(404);
+                  }
          }
-      }
    });
 
+// ---------CREATE
 
-//Aqui é onde seria quando queremos clicar e comprar o livro escolhido
-// app.get('/artigos/id', function (req, res) {
-//     res.render('artigosId');
-// });
+// criando um novo artigo com o método de requisição POST
+app.post("/artigos", (req, res) => {
+        const {
+            titulo,
+            categoria,
+            desc,
+            data,
+            hora,
+            autor, }
+            = req.body;
+    DB.artigos.push({
+      id: Math.floor(Math.random() * 10 + 1),
+            titulo,
+            categoria,
+            desc,
+            data,
+            hora,
+            autor,
+    });
+  res.send({ message: "Este novo artigo foi adicionado com sucesso!" });
+});
+
+// criando um novo autor com o método de requisição POST
+app.post("/autores", (req, res) => {
+        const {
+            nome,
+            email,
+            gravatar,
+            perfil,
+            categoria, }
+            = req.body;
+    DB.autores.push({
+      id: Math.floor(Math.random() * 10 + 1),
+            nome,
+            email,
+            gravatar,
+            perfil,
+            categoria,
+    });
+  res.send({ message: "Este novo autor foi adicionado com sucesso!" });
+});
 
 
+// ----------UPDATE
 
-
-app.listen(5000, (erro) => {
-    if (erro) {
-        console.log(erro, 'Erro');
+// atualizando um artigo ja existente pelo id, com auxilio do método PUT.
+app.put("artigos/:artigoId", (req, res) => {
+    const idArtigo = req.params.artigoId;
+    if (isNaN(idArtigo)) {
+      res.statusCode = 400;
+      res.send("O id informado não é um número.");
     } else {
-        console.log(' >>> WebNews está funcionando nesta porta: http://localhost:5000');
+      const id = parseInt(idArtigo);
+      const artigo = DB.artigos.findIndex((index) => index.id === id);
+      if (artigo === -1) {
+        res.sendStatus(404);
+      } else {
+        const {
+            titulo,
+            categoria,
+            desc,
+            data,
+            hora,
+            autor, }
+            = req.body;
+        DB.artigos.splice(artigo, 1, {
+            titulo,
+            categoria,
+            desc,
+            data,
+            hora,
+            autor, 
+        });
+        res.statusCode = 200;
+        res.json({ message: "Este artigo foi atualizado com sucesso!" });
+      }
+    }
+});
+
+// atualizando um autor ja existente pelo id, com auxilio do método PUT.
+app.put("/autores/:autorId", (req, res) => {
+    const idAutor = req.params.autorId;
+    if (isNaN(idAutor)) {
+      res.statusCode = 400;
+      res.send("O id informado não é um número.");
+    } else {
+      const id = parseInt(idAutor);
+      const autor = DB.autores.findIndex((index) => index.id === id);
+      if (autor === -1) {
+        res.sendStatus(404);
+      } else {
+        const {
+            nome,
+            email,
+            gravatar,
+            perfil,
+            categoria, }
+            = req.body;
+        DB.autores.splice(autor, 1, {
+            nome,
+            email,
+            gravatar,
+            perfil,
+            categoria,
+        });
+        res.statusCode = 200;
+        res.json({ message: "Este autor foi atualizado com sucesso!" });
+      }
     }
 });
 
 
-//app.post('/realizar-pedido')  //-> salvar as informações no banco'
+// -----------DELETE
+
+// deletando um artigo do servidor com o método DELETE.
+app.delete("artigos/:artigoId", (req, res) => {
+    const idArtigo = req.params.artigoId;
+    if (isNaN(idArtigo)) {
+      res.statusCode = 400;
+      res.send("O id informado não é um número.");
+    } else {
+      const id = parseInt(idArtigo);
+      const artigo = DB.artigos.findIndex((index) => index.id === id);
+      if (artigo === -1) {
+        res.sendStatus(404);
+      } else {
+        DB.artigos.splice(artigo, 1);
+        res.statusCode = 200;
+        res.json({ message: "Este artigo foi removido com sucesso!" });
+      }
+    }
+});
+
+// deletando um autor do servidor com o método DELETE.
+app.delete("/autores/:autorId", (req, res) => {
+    const idAutor = req.params.autorId;
+    if (isNaN(idAutor)) {
+      res.statusCode = 400;
+      res.send("O id informado não é um número.");
+    } else {
+      const id = parseInt(idAutor);
+      const autor = DB.autores.findIndex((index) => index.id === id);
+      if (autor === -1) {
+        res.sendStatus(404);
+      } else {
+        DB.autores.splice(autor, 1);
+        res.statusCode = 200;
+        res.json({ message: "Este autor foi removido com sucesso!" });
+      }
+    }
+});
+
+
+
+// Batendo na porta 5000 PARA INICIALIZAR O APP!
+app.listen(5000, (erro) => {
+    if (erro) {
+        console.log(erro, 'Erro');
+    } else {
+        console.log(' >>> O WebNews está funcionando nesta porta: http://localhost:5000');
+    }
+});
+
+
 
